e43735e5ff3ab3ac9e87deec7fd76ffc7807c17a
jdyeakel <jdyeakel@gmail.com>
2015-03-19 19:13:00 -0600

    Added alternative Dirichlet approach to integrating prey into diet

--- consumer_sim.R
+++ consumer_sim.R
@@ -1,5 +1,8 @@
+rm(list=c(ls()))
+
 #Simulated consumer code
 library(ellipse)
+library(gtools)
 library(RColorBrewer)
 
 
@@ -44,6 +47,14 @@ n_m[,1] <- n_init
 #theta <- rep(1,N)
 theta <- rep(1,N)
 
+#Dirichlet distribution for p_vector
+#p_vector is the proportional contribution of prey to the diet of consumer
+#p_vector determines a per-diem vector of proportional contributions
+#given the Dirichlet from which it is drawn
+Dir_param <- matrix(1,N,nprey-1)
+
+Dir_param[1,5:6] <- 100
+
 #Which prey item does each consumer specialize on?
 s_prey <- sample(nprey,N,replace=TRUE)
 #cumulative distribution
@@ -66,30 +77,46 @@ for (t in 1:(t_term-1)) {
     #With probability equal to e_gen[i], they will specialize
     #With probability equal to 1-e_gen[i], they will draw from prey randomly
     
+    #RANDOM DRAW VERSION
     #Draw random value
-    rdraw <- runif(1)
-    if (rdraw < theta[i]) {
-      #If specialist, select it's preferred prey
-      next_prey <- s_prey[i]
-    } else {
-      #If generalist, randomly select from all prey
-      next_prey <- sample(nprey,1,replace=TRUE)
-    }
+#     rdraw <- runif(1)
+#     if (rdraw < theta[i]) {
+#       #If specialist, select it's preferred prey
+#       next_prey <- s_prey[i]
+#     } else {
+#       #If generalist, randomly select from all prey
+#       next_prey <- sample(nprey,1,replace=TRUE)
+#     }
+#     #Randomly draw prey isotope values from known mean and sd
+#     cp_mean <- prey$CM[next_prey]
+#     cp_sd <- prey$CSD[next_prey]
+#     cp <- rnorm(1,cp_mean,cp_sd)
+#     np_mean <- prey$NM[next_prey]
+#     np_sd <- prey$NSD[next_prey]
+#     np <- rnorm(1,np_mean,np_sd)
+    
+    
+    #DIRICHLET VERSION
+    #draw proportional contribution vector from random dirichlet
+    p_vec <- numeric(nprey)
+    #if (N == 1) {
+    #  p_vec[1:nprey-1] <- rdirichlet(1,Dir_param)
+    #} else {
+    p_vec[1:nprey-1] <- rdirichlet(1,Dir_param[i,])
+    #}
+    p_vec[nprey] <- 1 - sum(p_vec)
+    #Draw prey values from each prey
+    cp_vec <- sapply(seq(1,nprey),function(x){rnorm(1,prey$CM[x],prey$CSD[x])})
+    np_vec <- sapply(seq(1,nprey),function(x){rnorm(1,prey$NM[x],prey$NSD[x])})
+    #Calculate weighted average
+    cp <- p_vec %*% cp_vec
+    np <- p_vec %*% np_vec
     
     #Prey biomass
     #set to one if each prey is to be equally weighted
     #(assume 1 kg of each thing is eaten rather than at individual level)
     mp <- 1 #prey$Biomass[next_prey]
-    
-    #Randomly draw prey isotope values from known mean and sd
-    cp_mean <- prey$CM[next_prey]
-    cp_sd <- prey$CSD[next_prey]
-    cp <- rnorm(1,cp_mean,cp_sd)
-    
-    np_mean <- prey$NM[next_prey]
-    np_sd <- prey$NSD[next_prey]
-    np <- rnorm(1,np_mean,np_sd)
-    
+
     #weights for body size
     f <- mb/(mb + mp)
     
@@ -120,14 +147,15 @@ lines(c_m[ind,],n_m[ind,],pch=16,cex=0.25,col=colors[i])
 ind <- 1
 plot(c_m[ind,],pch=16,cex=0.25) #; lines(c_m[ind,])
 
-par(mfrow=c(2,1))
 
+#CARBON
 #Analytical approximation for pure specialist, SD=0
+par(mfrow=c(2,1))
 analyticE <- sapply(seq(1,t_term),function(x){f^x*(c_init - cp_mean) + cp_mean})
 plot(c_m[ind,2:10000],pch=16,cex=0.5,xlab="time",ylab="d13C",col="gray")
 lines(analyticE)
 
-binsize = 1000
+binsize = 200
 analyticSD <- sapply(seq(1,t_term),function(x){sqrt(0.5*cp_sd^2*(f-1)*(exp(2*(f-1)*x)-1))})
 bins <- seq(binsize+1,t_term,by=binsize)
 sd_bin <- numeric(length(bins)-1)
@@ -139,8 +167,29 @@ for (i in 1:(length(bins)-1)) {
 #sapply(seq(binsize+1,t_term,by=binsize),function(x){sd(c_m[ind,x-binsize:x])})
 #I don't know why ^ doesn't work!
 plot(bins[-1],sd_bin,
-     pch=16,cex=0.5,xlim=c(0,tail(bins,n=1)),ylim=c(0,0.5),xlab="time",ylab="d13C",col="gray")
+     pch=16,cex=0.5,xlim=c(0,tail(bins,n=1)),ylim=c(0,0.5),xlab="time",ylab="d13C SD",col="black")
 lines(analyticSD)
 
 
+#NITROGEN
+#Analytical approximation for pure specialist, SD=0
+par(mfrow=c(2,1))
+analyticE <- sapply(seq(1,t_term),function(x){f^x*(n_init - np_mean) + np_mean})
+plot(n_m[ind,2:t_term],pch=16,cex=0.5,xlab="time",ylab="d15N",col="gray")
+lines(analyticE)
+
+binsize = 200
+analyticSD <- sapply(seq(1,t_term),function(x){sqrt(0.5*np_sd^2*(f-1)*(exp(2*(f-1)*x)-1))})
+bins <- seq(binsize+1,t_term,by=binsize)
+sd_bin <- numeric(length(bins)-1)
+for (i in 1:(length(bins)-1)) {
+  bin0 <- bins[i]
+  bin1 <- bins[i+1]
+  sd_bin[i] <- sd(n_m[ind,bin0:bin1])
+}
+#sapply(seq(binsize+1,t_term,by=binsize),function(x){sd(c_m[ind,x-binsize:x])})
+#I don't know why ^ doesn't work!
+plot(bins[-1],sd_bin,
+     pch=16,cex=0.5,xlim=c(0,tail(bins,n=1)),ylim=c(0,0.5),xlab="time",ylab="d15N SD",col="black")
+lines(analyticSD)
 
